910077e7d6811859aafbaff1b7cb54a88539241a
Cameron McKay <cameron@webnx.com>
2022-12-12 23:43:33 -0700

    Parts can be added and removed from assets

---
+++ src/views/InventoryView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import CartItemComponent from '../components/CartItemComponent.vue';
+
+interface Props {
+    http: AxiosInstance,
+    store: Store<UserState>,
+    router: Router,
+    errorHandler: (err: Error | AxiosError | string) => void,
+    displayMessage: (message: string) => void
+}
+// Get global objects from props
+const { http, store, router, errorHandler, displayMessage } = defineProps<Props>()
+
+</script>
+<template>
+    <div>
+        <h1>{{ store.state.user.first_name }}'s Inventory'</h1>
+        
+    </div>
+</template>
