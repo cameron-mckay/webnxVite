d352cb70a972a5eae8306c5217902506dd686f62
Cameron McKay <cameron@webnx.com>
2023-03-22 21:59:55 -0600

    Block google search indexing and remove unnecessary shims

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,20 +1,7 @@
 /// <reference types="vite/client" />
 
-import { AxiosInstance } from "axios";
-import { RouteLocation, Router } from "vue-router";
-import { Store } from "vuex";
-
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
-}
-
-declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-    $store: Store;
-    $router: Router;
-    $route: RouteLocation;
-  }
 }
