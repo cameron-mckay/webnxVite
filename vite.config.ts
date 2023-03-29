dd2b71f6717bcc145b747739fe36770cef8b1a0c
Cameron McKay <cameron@webnx.com>
2023-03-28 18:06:11 -0600

    Improved component chunking

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,43 @@ export default defineConfig({
     host: true,
   },
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#outputmanualchunks
+      output: {
+        manualChunks: {
+          'group-login': [
+            './src/views/LoginView.vue',
+            './src/views/RegisterView.vue',
+          ],
+          'group-essentials': [
+            './src/views/Assets/AssetSearchView.vue',
+            './src/views/Parts/FindPartView.vue',
+            './src/views/Parts/InventoryView.vue'
+          ],
+          'group-asset-extras': [
+            './src/views/Assets/AssetView.vue',
+            './src/views/Assets/AddUntrackedAssetView.vue',
+            './src/views/Assets/EditAssetView.vue',
+          ],
+          'group-part-extras': [
+            './src/views/Parts/PartHistoryView.vue',
+            './src/views/Parts/PartView.vue'
+          ],
+          'group-kiosk': [
+            './src/views/Parts/CartView.vue',
+            './src/views/Parts/CheckInView.vue',
+          ],
+          'group-admin': [
+            './src/views/Admin/AddToInventoryView.vue',
+            './src/views/Admin/AdminDashboardView.vue',
+            './src/views/Admin/AdminPartSearchView.vue',
+            './src/views/Admin/AdminUserView.vue',
+            './src/views/Admin/CreatePartView.vue',
+            './src/views/Admin/InventoryManagerDashboardView.vue'
+          ]
+        },
+      },
+    },
+  },
 });
