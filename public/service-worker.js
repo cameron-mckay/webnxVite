ce827998590da1e7f6494abe25da665c34db5f6d
Cameron <cameron@webnx.com>
2024-02-14 15:57:29 -0700

    Fixed subscription object

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,9 +20,9 @@ self.addEventListener('push', (event) => {
   event.waitUntil(
     // Show a notification with title 'ServiceWorker Cookbook' and use the payload
     // as the body.
-    self.registration.showNotification('ServiceWorker Cookbook', {
-      body: payload,
-    })
+    // self.registration.showNotification('ServiceWorker Cookbook', {
+    //   body: payload,
+    // })
   );
 })
 
